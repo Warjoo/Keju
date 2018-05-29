a97e4e51dd31b52e0c2d890c292e16346e644f34
ahmad5325 <syafrizal5325@gmail.com>
2018-05-29 15:10:51 +0700

    Test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,Response,jsonify
 from peneliti import namapeneliti as nmpl
-import scholarly.json
+import scholarly,json
 
 app = Flask(__name__)
 
@@ -13,22 +13,12 @@ def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
-@app.route('/crot', methods=['POST'])
-def login():
-	return request.form['anu']
+#@app.route('/crot', methods=['POST'])
+#def login():
+#	return request.form['anu']
 
-@app.route('/peneliti/<Chris>', methods=[POST])
-def Coba(Chris)
-    return str(nmpl.C(Chris))
-@app.route('/peneliti/<Juliffer>', methods=[POST])
-def Coba1(Juliffer)
-    return str(nmpl.D(Juliffer))
-@app.route('/peneliti/<Roza>', methods=[POST])
-def Coba2(Roza)
-    return str(nmpl.E(Roza))
-@app.route('/peneliti/<Manish Singh>', methods=[POST])
-def Coba3(Manish Singh)
-    return str(nmpl.A(Manish Singh))
-@app.route('/peneliti/<Oussama Khatib>', methods=[POST])
-def Coba4(Oussama Khatib)
-    return str(nmpl.B(Oussama Khatib))
+@app.route('/peneliti/<Rutgers, 3349>',methods=['POST'])
+def cobaaja(Rutgers, 3349):
+		search_query = scholarly.search_author('Rutgers, 3349')
+		return str(next(search_query))
+
--- peneliti/namapeneliti.py
+++ peneliti/namapeneliti.py
@@ -1,33 +1,6 @@
 import scholarly
+def cobaaja(Rutgers, 3349):
+		search_query = scholarly.search_author('Rutgers, 3349')
+		return next(search_query)
 
-def A(Manish Singh):
-        search_query = scholarly.search_author('Manish Singh')
-	print(next(search_query))
-        {'_filled': False,
-         'affiliation': 'Rutgers University, New Brunswick, NJ',
-         'citedby': 3349,
-         'email': '@ruccs.rutgers.edu',
-         'id': '9XRvM88AAAAJ',
-         'interests': ['Human perception', 'Computational Vision', 'Cognitive Science'],
-         'name': 'Manish Singh',
-         'url_picture': '/citations/images/avatar_scholar_56.jpg'}
-def B(Oussama Khatib):
-        search_query = scholarly.search_author('Oussama Khatib')      
-	print(next(search_query))
-        {'_filled': False,
-         'affiliation': 'Stanford University',
-         'citedby': 27493,
-         'email': '@cs.stanford.edu',
-         'id': '4arkOLcAAAAJ',
-         'interests': ['Robotics', 'Haptics', 'Human Motion'],
-         'name': 'Oussama Khatib',
-         'url_picture': '/citations/images/avatar_scholar_56.jpg'}
-def C(Chris):
-        search_query = scholarly.search_author('Chris')
-	return next(search_query)
-def D(Juliffer):
-	search_query = scholarly.search_author('Juliffer')
-	return next(search_query)
-def E(Roza):
-	search_query = scholarly.search_author('Roza')
-	return next(search_query)
+
