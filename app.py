a04ec1fc10f8e089951b946b2fe08b31f8400e56
ahmad5325 <syafrizal5325@gmail.com>
2018-05-30 22:42:28 +0700

    Test

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask,request,Response,jsonify
+from flask import Flask,request,jsonify
 from peneliti import namapeneliti as nmpl
 import scholarly,json
+from flask_mysqldb import MySQL 
+import pymysql
+
+db = pymysql.connect("localhost", "root", "", "affiliasicitedby")
 
 app = Flask(__name__)
+mysql = MySQL(app)
 
 @app.route('/<gurih>')
 def hello_world(gurih):
@@ -13,11 +18,22 @@ def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
-#@app.route('/crot', methods=['POST'])
-#def login():
-#	return request.form['anu']
+@app.route('/crot', methods=['POST'])
+def login():
+	return request.form['anu']
 
 @app.route('/peneliti/<Rutgers>',methods=['POST'])
 def coba(Rutgers):
-		return str(ml.cobaaja('Rutgers'))
+		search_query = scholarly.search_author('Rutgers')
+		return str(next(search_query))
+
+@app.route('/aff')
+def bisa():
+	cursor = db.cursor()
+	sql = "SELECT * FROM searchpost"
+	cursor.execute(sql)
+	results = cursor.fetchall()
+	return str(results)
+
+
 
