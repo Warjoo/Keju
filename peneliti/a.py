041421cc465cec6800d082e0cb7d9f606db8f575
nsfthrn <nsfthrn@gmail.com>
2018-06-01 23:30:41 +0700

    Ini bagian Nisa, pak!

--- peneliti/a.py
+++ peneliti/a.py
@@ -5,5 +5,9 @@ def world():
 # Define a variable
 shark = "warjo"
 
+# Define a class
+class Octopus:
+    def __init__(self, name, color):
+
 #def tell_me_about_the_octopus(self):
         print("This octopus is " + self.color + "pink")
