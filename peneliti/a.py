65859359bbe6d60fe4e56a4a8355f734fa49cd35
nsfthrn <nsfthrn@gmail.com>
2018-06-02 21:56:37 +0700

    Ini bagian Nisa, pak!

--- peneliti/a.py
+++ peneliti/a.py
@@ -8,6 +8,7 @@ shark = "warjo"
 # Define a class
 class Octopus:
     def __init__(self, name, color):
+    	self.color = color
 
 #def tell_me_about_the_octopus(self):
         print("This octopus is " + self.color + "pink")
