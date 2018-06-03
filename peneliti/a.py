7eaac35e8b05532caae2f2040cf8da53ec2b80db
nsfthrn <nsfthrn@gmail.com>
2018-06-03 22:12:25 +0700

    Ini bagian Nisa, pak!

--- peneliti/a.py
+++ peneliti/a.py
@@ -9,6 +9,7 @@ shark = "warjo"
 class Warjoku:
     def __init__(self, name, color):
     	self.color = color
+    	self.name = name
 
 #def tell_me_about_the_octopus(self):
         print("Kelompok ini adalah " + self.color + "Warjoo")
